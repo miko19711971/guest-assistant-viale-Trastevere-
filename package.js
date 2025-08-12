8b0c2fc9ea1074aec4ab43a5ce1aa183ac4074b1
miko19711971 <mikbondi@gmail.com>
2025-08-12 17:00:00 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "guest-assistant-vialetrastevere108",
+  "version": "1.0.0",
+  "description": "Guest Assistant web app for Viale Trastevere 108, Rome",
+  "main": "index.js",
+  "type": "module",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.19.2",
+    "openai": "^4.0.0"
+  },
+  "engines": {
+    "node": ">=18.x"
+  },
+  "license": "MIT"
+}
